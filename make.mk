4f18e6182f003d8fa41018b6a4934fe95c9a2c48
chenyutao36 <chenyutao890704@gmail.com>
2025-02-27 14:45:16 +0800

    Fix for linux system

--- make.mk
+++ make.mk
@@ -2,18 +2,20 @@ CC=gcc
 CPP=g++
 AR=ar
 CPPFLAGS = -Wall -O3 -march=native -mfpmath=sse -fPIC -std=c++11
-#CPPFLAGS_MAIN = -Wall -march=native -mfpmath=sse -pedantic -Wshadow -Wfloat-equal -O3 -Wconversion -Wsign-conversion -finline-functions -fPIC -DLINUX -D__NO_COPYRIGHT__
 ARFLAGS = rcs
 
-QPOASES_PATH = /home/chen/Documents/Packages/qpOASES-3.2.0
+QPOASES_PATH = /home/cyt/Documents/qpOASES-3.2.0
+EIGEN_PATH = /home/cyt/Documents/eigen-3.4.0
+LINALG_PATH = /usr/lib/x86_64-linux-gnu
 CPPMPC_PATH  = $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 
+EIGEN_HEADER = $(EIGEN_PATH)
 QPOASES_HEADER = $(QPOASES_PATH)/include
 QPOASES_LIB  = $(QPOASES_PATH)/bin
 CPPMPC_HEADER = $(CPPMPC_PATH)/include
 CPPMPC_LIB  = $(CPPMPC_PATH)/lib
 QPOASES_LINK = -L$(QPOASES_LIB) -Wl,-rpath=$(QPOASES_LIB) -lqpOASES
-LINK_DEPENDS = -llapack -lblas -lm 
+LINK_DEPENDS = -L$(LINALG_PATH)/ -llapack -lblas -lm 
 CPPMPC_LINK = -L$(CPPMPC_LIB) -Wl,-rpath=$(CPPMPC_LIB) -lcppmpc
 MODEL_HEADER = $(CPPMPC_PATH)/model
 MODEL_LINK = -L$(CPPMPC_PATH)/model -Wl,-rpath=$(CPPMPC_PATH)/model -lmymodel
